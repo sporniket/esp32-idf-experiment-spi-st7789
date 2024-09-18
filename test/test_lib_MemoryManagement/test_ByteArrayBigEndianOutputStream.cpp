dee390e94167fe2d62b1dfd565525c1e708a80f5
David SPORN <sporniket.studio@gmail.com>
2024-09-18 06:14:22 +0200

    Memory management : test suite runs and passes

--- test/test_lib_MemoryManagement/test_ByteArrayBigEndianOutputStream.cpp
+++ test/test_lib_MemoryManagement/test_ByteArrayBigEndianOutputStream.cpp
@@ -12,67 +12,66 @@ void setUp(void) {}
  */
 void tearDown(void) {}
 
-void test_ByteArrayBigEndianOutputStream__can_stream_values() {
-    uint8_t bufferRaw[5] MemoryArea buffer(buffer, sizeof(buffer))
+uint8_t bufferRaw[5];
+MemoryArea buffer(bufferRaw, sizeof(bufferRaw));
 
-            ByteArrayBigEndianOutputStream ostream(buffer);
-    uint8_t one = 1;
-    uint16_t two = 2;
-    uint32_t three = 3;
+uint8_t one = 1;
+uint16_t two = 2;
+uint32_t three = 3;
+
+void test_ByteArrayBigEndianOutputStream__can_stream_values() {
+    ByteArrayBigEndianOutputStream ostream(buffer);
 
     ostream << one << two;
-    TEST_ASSERT_EQUAL_INT(ostream.status(), ByteArrayStreamStatus::E_OK);
-    TEST_ASSERT_EQUAL_INT(ostream.length(), 3);
-    TEST_ASSERT_EQUAL_INT(1, bufferRaw[0]);
-    TEST_ASSERT_EQUAL_INT(0, bufferRaw[1]);
-    TEST_ASSERT_EQUAL_INT(2, bufferRaw[2]);
+    TEST_ASSERT_TRUE((ostream.getStatus() == ByteArrayStreamStatus::E_OK));
+    TEST_ASSERT_TRUE((ostream.getLength() == 3));
+    TEST_ASSERT_TRUE((1 == bufferRaw[0]));
+    TEST_ASSERT_TRUE((0 == bufferRaw[1]));
+    TEST_ASSERT_TRUE((2 == bufferRaw[2]));
 
     ostream.reset();
     ostream << three;
-    TEST_ASSERT_EQUAL_INT(ostream.status(), ByteArrayStreamStatus::E_OK);
-    TEST_ASSERT_EQUAL_INT(ostream.length(), 4);
-    TEST_ASSERT_EQUAL_INT(0, bufferRaw[0]);
-    TEST_ASSERT_EQUAL_INT(0, bufferRaw[1]);
-    TEST_ASSERT_EQUAL_INT(0, bufferRaw[2]);
-    TEST_ASSERT_EQUAL_INT(3, bufferRaw[2]);
+    TEST_ASSERT_TRUE((ostream.getStatus() == ByteArrayStreamStatus::E_OK));
+    TEST_ASSERT_TRUE((ostream.getLength() == 4));
+    TEST_ASSERT_TRUE((0 == bufferRaw[0]));
+    TEST_ASSERT_TRUE((0 == bufferRaw[1]));
+    TEST_ASSERT_TRUE((0 == bufferRaw[2]));
+    TEST_ASSERT_TRUE((3 == bufferRaw[3]));
 }
 
 void test_ByteArrayBigEndianOutputStream__is_in_error_when_pushing_more_than_it_can_store() {
-    uint8_t bufferRaw[5] MemoryArea buffer(buffer, sizeof(buffer))
-
-            ByteArrayBigEndianOutputStream ostream(buffer);
-    uint8_t one = 1;
-    uint16_t two = 2;
-    uint32_t three = 3;
-
+    ByteArrayBigEndianOutputStream ostream(buffer);
+    
     ostream << one << two << three;
-    TEST_ASSERT_EQUAL_INT(ostream.status(), ByteArrayStreamStatus::E_KO__OUT_OF_BOUND);
-    TEST_ASSERT_EQUAL_INT(ostream.length(), 3);
+    TEST_ASSERT_TRUE((ostream.getStatus() == ByteArrayStreamStatus::E_KO__OUT_OF_BOUND));
+    TEST_ASSERT_TRUE((ostream.getLength() == 3));
 }
 
 void test_ByteArrayBigEndianOutputStream_reset__goes_back_to_the_start() {
-    uint8_t bufferRaw[5] MemoryArea buffer(buffer, sizeof(buffer))
-
-            ByteArrayBigEndianOutputStream ostream(buffer);
-    uint8_t one = 1;
-    uint16_t two = 2;
-
+    ByteArrayBigEndianOutputStream ostream(buffer);
+    
     ostream << one << two;
     ostream.reset();
-    TEST_ASSERT_EQUAL_INT(ostream.status(), ByteArrayStreamStatus::E_OK);
-    TEST_ASSERT_EQUAL_INT(ostream.length(), 0);
+    TEST_ASSERT_TRUE((ostream.getStatus() == ByteArrayStreamStatus::E_OK));
+    TEST_ASSERT_TRUE((ostream.getLength() == 0));
 }
 
 void test_ByteArrayBigEndianOutputStream_reset__reinstate_ok_state() {
-    uint8_t bufferRaw[5] MemoryArea buffer(buffer, sizeof(buffer))
-
-            ByteArrayBigEndianOutputStream ostream(buffer);
-    uint8_t one = 1;
-    uint16_t two = 2;
-    uint32_t three = 3;
-
+    ByteArrayBigEndianOutputStream ostream(buffer);
+    
     ostream << one << two << three;
     ostream.reset();
-    TEST_ASSERT_EQUAL_INT(ostream.status(), ByteArrayStreamStatus::E_OK);
-    TEST_ASSERT_EQUAL_INT(ostream.length(), 0);
+    TEST_ASSERT_TRUE((ostream.getStatus() == ByteArrayStreamStatus::E_OK));
+    TEST_ASSERT_TRUE((ostream.getLength() == 0));
+}
+
+int main(int argc, char **argv) {
+    ByteArrayBigEndianOutputStream ostream(buffer);
+    
+    UNITY_BEGIN();
+    RUN_TEST(test_ByteArrayBigEndianOutputStream__can_stream_values);
+    RUN_TEST(test_ByteArrayBigEndianOutputStream__is_in_error_when_pushing_more_than_it_can_store);
+    RUN_TEST(test_ByteArrayBigEndianOutputStream_reset__goes_back_to_the_start);
+    RUN_TEST(test_ByteArrayBigEndianOutputStream_reset__reinstate_ok_state);
+    UNITY_END();
 }
